582c42863e45db498b8e34fa21c779e961e9c23a
Anshuman-Programmer <anshumantalukdar02@gmail.com>
2022-10-11 01:07:33 +0530

    Header done

---
+++ src/components/header/Header.js
@@ -0,0 +1,31 @@
+
+import React, { useState } from 'react'
+import styles from './_header.module.scss'
+
+const Header = () => {
+
+  const [activeNav, setactiveNav] = useState("home")
+
+  const handleClick = (e) => {
+    setactiveNav(e.target.id)
+  }
+
+  return (
+    <nav className={styles.header}>
+      <h3 className={styles.logo}>
+        Anshuman
+      </h3>
+      <ul className={styles.links}>
+        <li id="home" className={`${styles.link} ${activeNav === "home" && styles.active}`} onClick={handleClick}>Home</li>
+        <li id="about" className={`${styles.link} ${activeNav === "about" && styles.active}`} onClick={handleClick}>About</li>
+        <li id="faqs" className={`${styles.link} ${activeNav === "faqs" && styles.active}`} onClick={handleClick}>FAQs</li>
+        <li id="usecases" className={`${styles.link} ${activeNav === "usecases" && styles.active}`} onClick={handleClick}>Use Cases</li>
+      </ul>
+      <button className={styles.button}>
+        Account
+      </button>
+    </nav>
+  )
+}
+
+export default Header
